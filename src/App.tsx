670cb61c4d5e7ad8944b1d5c90eed4065abcc4a9
piyushh-11 <115842755+piyushh-11@users.noreply.github.com>
2025-01-06 23:22:21 -0600

    real tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
-import './index.css';
 import './App.css'
 
 function App() {
-
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <div className="bg-gradient-to-r from-sky-500 to-indigo-500">
+      <h1 className="text-3xl font-bold">
+        Hello, Tailwind CSS!
+      </h1>
+    </div>
+  );
 }
 
 export default App
