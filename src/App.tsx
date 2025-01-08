f592bbafbc48fb15b4de2feea5d88afcf30bbb21
piyushh-11 <115842755+piyushh-11@users.noreply.github.com>
2025-01-08 17:10:45 -0600

    side bar complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Sidebar from "./components/Sidebar";
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Blogs from './pages/Blogs';
+import Photos from './pages/Photos';
 import './App.css'
 
 function App() {
   return (
-    <div className="flex">
-      <Sidebar />
-    </div>
+    <Router>
+      <div className="flex">
+        <Sidebar />
+        <main className="w-4/5 h-max">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/blogs" element={<Blogs />} />
+            <Route path="/photos" element={<Photos />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,10 +1,52 @@
-import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
+import { Linkedin, GitHub } from 'react-feather'
 import Polaroid from './Polaroid'
 
 const Sidebar = () => {
   return (
-    <div className='flex flex-col justify-evenly items-center p-4'>
-        <Polaroid />
+    <div className='flex flex-col justify-top space-y-8 items-center pt-16 w-1/5 h-screen'>
+      <Polaroid />
+      <nav className='flex flex-col items-center'>
+        {[
+          { to: '/', icon: '🏡.png', label: 'Home' },
+          { to: '/projects', icon: '👨‍💻.png', label: 'Projects' },
+          { to: '/blogs', icon: '📝.png', label: 'Blogs' },
+          { to: '/photos', icon: '📸.png', label: 'Photos' },
+        ].map((link) => (
+            <NavLink 
+            key={link.to} 
+            to={link.to} 
+            end 
+            className={({ isActive }) => 
+              `flex flex-row justify-between items-center p-1 w-32 text-slate-400 font-nunito ${isActive ? 'font-bold text-slate-900' : ''} hover:scale-105 transition-transform duration-200`
+            }
+            >
+            <img className="w-6" src={`src/assets/icons/${link.icon}`} alt={link.label} />
+            <span>{link.label}</span>
+            </NavLink>
+        ))}
+      </nav>
+      <div>
+        <NavLink 
+          to='https://www.linkedin.com/in/piyush-hole' 
+          className='flex flex-row justify-between items-center p-1 w-32 font-nunito text-blue-600 hover:scale-105 transition-transform duration-200' 
+          target='_blank' 
+          rel='noopener noreferrer'
+        >
+          <Linkedin strokeWidth={1.5} className="w-6" />
+          <span>LinkedIn</span>
+        </NavLink>
+        <NavLink 
+          to='https://github.com/piyushh-11' 
+          className='flex flex-row justify-between items-center p-1 w-32 font-nunito hover:scale-105 transition-transform duration-200' 
+          target='_blank' 
+          rel='noopener noreferrer'
+        >
+          <GitHub strokeWidth={1.5} className="w-6" />
+          <span>GitHub</span>
+        </NavLink>
+        
+      </div>
     </div>
   )
 }
